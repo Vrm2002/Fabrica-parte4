202c680fd79df9f08af56c634aa9ba8dc482effa
Gabriel Gravena Barros <129797873+Vrm2002@users.noreply.github.com>
2023-06-19 17:29:33 -0400

    adicionado exercicios novos
    aaaaa

--- anotaçõesFunçõesCriar.py
+++ anotaçõesFunçõesCriar.py
@@ -24,3 +24,26 @@ def soma(x,y):
     result=x+y
     return result 
 #Return serve para colocar uma saida para a função. Para no caso de querer manipular o retorno dessa função.
+
+def invert(nome,sobrenome):
+    nomeInverse=sobrenome+", "+nome
+    return nomeInverse
+nome=input("Nome: ")
+sobrenome=input("Sobrenome: ")
+inverse=invert(nome,sobrenome)
+print("Olá", inverse)
+#função que inverte nome e sobrenome de lugares.
+
+def parimpar(num):
+    result=num%2
+    if result==0:
+        return True
+    else:
+        return False 
+while True:
+    num=int(input("Digite um número: "))
+    if parimpar(num):
+        print("É par")
+    else:
+        print("É impar")
+#O código acima confirma se um número é par ou não usando funções.
