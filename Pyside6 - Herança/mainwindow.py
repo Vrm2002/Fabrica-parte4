5da7f981db6159c6a81fc847bae4881d11cb9ce9
Gabriel Gravena Barros <129797873+Vrm2002@users.noreply.github.com>
2023-08-09 17:48:36 -0400

    adicionando exercicio incompleto
    a

--- Pyside6 - Herança/mainwindow.py
+++ Pyside6 - Herança/mainwindow.py
@@ -1,5 +1,7 @@
 import sys
 from PySide6.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QVBoxLayout
+from PySide6.QtGui import QPixmap
+from rectanglewindow import RectWindow
 
 
 class MainWindow(QMainWindow):
@@ -8,12 +10,10 @@ class MainWindow(QMainWindow):
 
 
         self.setFixedSize(800,600)
-        self.UI()
+        self.ui()
 
     
-    def UI(self):
-
-
+    def ui(self):
         self.setWindowTitle("App - Calculo de Área")
 
 
@@ -30,15 +30,42 @@ class MainWindow(QMainWindow):
 
         self.button = QPushButton("Retângulo",self)
         self.button.setGeometry(150,200,150,40)
+        self.button.clicked.connect(self.rectan_window_connect)
         layout.addWidget(self.button)
 
         
         self.button2 = QPushButton("Círculo",self)
         self.button2.setGeometry(480,200,150,40)
+        self.button2.clicked.connect(self.circle_window_connect)
         layout.addWidget(self.button2)
 
 
+        image_lbl = QLabel(self)
+        pixmap = QPixmap("circulo.png")
+        image_lbl.setPixmap(pixmap)
+        image_lbl.setGeometry(480,250,150,150)
+        image_lbl.setScaledContents(True)
+        layout.addWidget(image_lbl)
+
+
+        image_lbl2 = QLabel(self)
+        pixmap2 = QPixmap("retangulo.png")
+        image_lbl2.setPixmap(pixmap2)
+        image_lbl2.setGeometry(150,250,150,150)
+        image_lbl2.setScaledContents(True)
+        layout.addWidget(image_lbl2)
+
+
         self.setLayout = layout
+
+
+    def circle_window_connect(self):
+        pass
+
+
+    def rectan_window_connect(self):
+        self.rect_window = RectWindow()
+        self.rect_window.show
 
 
 if __name__ == "__main__":
