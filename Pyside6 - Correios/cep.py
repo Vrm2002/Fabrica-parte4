f009beef2debff51402e8a9fc71956d2f11fd8cd
Gabriel Gravena Barros <129797873+Vrm2002@users.noreply.github.com>
2023-08-11 17:08:10 -0400

    enviando arquivos
    a

---
+++ Pyside6 - Correios/cep.py
@@ -0,0 +1,20 @@
+import brazilcep
+from PySide6.QtWidgets import QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+
+        self.setWindowTitle('Busca - CEP')
+        self.CepUi(self)
+
+    
+    def CepUi(self):
+
+
+
+
+
+e
