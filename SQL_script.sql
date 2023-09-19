87bd54ff8848c404d0376cf68053b78bcb0a3078
Gabriel Gravena Barros <129797873+Vrm2002@users.noreply.github.com>
2023-09-19 15:06:38 -0400

    a
    a

--- SQL_script.sql
+++ SQL_script.sql
@@ -16,3 +16,5 @@ use senac; #Serve para usar um banco de dados
 create table alunos (id int auto_increment, nome varchar(30) not null, fone varchar(30) not null, email varchar(30) not null, primary key(id)); #Cria uma tabela com as seguintes colunas
 # primary key - chave unica de uma tabela, varchar(30) o 30 em varchar singifica o limite de caracteres, not null impede de uma coluna ficar vazia, auto_increment faz com que a coluna se encha sozinha
 describe alunos;
+
+constraint foreign key (atributo) references tabela2(atributo2)
